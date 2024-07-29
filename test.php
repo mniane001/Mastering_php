8bbe283ee13a5f8d9bf8db8b148fea72c4e70710
mniane001 <nianemouhamed100@gmail.com>
2024-07-29 11:14:35 -0400

    base php

--- test.php
+++ test.php
@@ -8,6 +8,26 @@ echo " assalamouhalaykoum aduna ! <br>" ;
 $tour  = "Mouhamed" ;
 $at = 26;
 
-echo "sama tour moy $tour te amna $at at ";
+echo "sama tour moy $tour te amna $at at <br>";
+
+//condition 
+$at = 26;
+if ($at >= 18){
+    echo "légui mague nga , meune nga voter <br>";
+}else{
+    echo "xalé nga, meunago voter <br>";
+}
+
+//Boucle
+for ($i = 1; $i<=5; $i++){
+    echo "numéro bi moy $i <br>";
+}
+
+//fonctions 
+function waxtane($tour){
+    echo "na nga def $tour, mba yaa nguissi diam";
+}
+
+echo waxtane ("Seynabou");
 
 ?>
